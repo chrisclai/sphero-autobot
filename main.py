e08c9368f5eca514d51e2750b636e47ab5970025
chrisclai <chrisclai02@gmail.com>
2021-01-21 19:38:52 -0800

    Update main.py
    Abbreviated redundant try-catch exceptions and corrected sensor assignment

--- main.py
+++ main.py
@@ -25,44 +25,18 @@ pca.set_addr_bit(0,0)
 avail_addresses = qwiic.scan()
 
 # Remove Pi Servo pHat from avail_address list
-try:
-    avail_addresses.remove(0x40)
-except ValueError:
-    print("Addresses for Pi Servo pHat (0x40) not in scanned addresses")
+avail_addresses.remove(0x40)
 
-# Enable channel 4 to change back sensor address from default 41 to 85
-# Also Enable channel 3 (front sensor)
-try:
-    mux.enable_channels(4)
-    mux.enable_channels(3)
-except Exception as e:
-    print(e)
-
-# Initialize address to be changed (41)
-print("Initializing Device")
+# Activate back sensor and change address
+mux.enable_channels(4)
 ToF_rear = qwiic.QwiicVL53L1X(41)
-try:
-    ToF_rear.SensorInit()
-except Exception as e:
-    if e == OSError or e == IOError:
-        print("Issue connecting to device.")
-        print(e)
-    else:
-        print(e)
-
-# Change address (41) to new one (85)
-try:
-    ToF_rear.setI2CAddress(85)
-except Exception as e:
-    if e == OSError or e == IOError:
-        print("Issue connecting to device")
-        print(e)
-    else:
-        print(e)
-print("Address has been successfully changed.")
+ToF_rear = SensorInit()
+ToF_rear.setI2CAddress(85)
 
 # Activate front sensor
+mux.enable_channels(3)
 ToF_front = qwiic.QwiicVL53L1X(41)
+TOF_front.SensorInit()
 
 while True:
     try:
